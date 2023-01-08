dda818060cac9924c67ef777642190a4b80dc3c0
Lucas Qu <u1330273@utah.edu>
2023-01-08 14:36:04 -0700

    day night cycle!

--- Assets/DayNightCycle.cs
+++ Assets/DayNightCycle.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class DayNightCycle : MonoBehaviour
 {
     [SerializeField] private Transform clockHandHolder;
+    [SerializeField] private float startAngle = -60f;
     [SerializeField] private float dayDuration = 120f;
 
     [SerializeField] private float startDelay = 2f;
@@ -14,13 +17,34 @@ public class DayNightCycle : MonoBehaviour
     private float duskAngle = -210f;
     private float nightAngle = -310f;
 
+    [SerializeField] private Volume volume;
+    // project range to 0 ~ 360 -> 0 ~3.6 to make life easier
+    [SerializeField] private AnimationCurve volumeCurve;
+
+    [SerializeField] private Light2D nightLight;
+    [SerializeField] private AnimationCurve lightCurve;
+
     private float accumulatedTime = 0f;
 
     private void Start()
     {
         // get rid of the smoothness
         //InvokeRepeating("UpdateCountDown", startDelay, 0.1f);
+        Init();
     }
+
+    private void Init()
+    {
+        float angle = startAngle % 360;
+        clockHandHolder.eulerAngles = new Vector3(0f, 0f, angle);
+
+        float projectedAngle = -1 * angle * 0.01f;
+        float ligthIntensity = volumeCurve.Evaluate(projectedAngle);
+        volume.weight = 1f - ligthIntensity;
+
+        nightLight.intensity = lightCurve.Evaluate(projectedAngle);
+    }
+
 
     private void Update()
     {
@@ -40,9 +64,21 @@ public class DayNightCycle : MonoBehaviour
         if(accumulatedTime >= dayDuration)
             accumulatedTime %= dayDuration;
 
-        float angle = -360f / dayDuration * accumulatedTime;
+        float angle = startAngle + (-360f / dayDuration * accumulatedTime);
         angle %= 360;
 
         clockHandHolder.eulerAngles = new Vector3(0f, 0f, angle);
+
+        // project to 0 ~ 3.6
+        float projectedAngle = -1 * angle * 0.01f;
+        float ligthIntensity = volumeCurve.Evaluate(projectedAngle);
+        volume.weight = 1f - ligthIntensity;
+
+        nightLight.intensity = lightCurve.Evaluate(projectedAngle);
+    }
+
+    private void OnValidate()
+    {
+        Init();
     }
 }
